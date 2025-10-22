6d6302d210db76fbd7d6ff3ebed7350e06dcce75
geon <leegeonhee0204@naver.com>
2025-10-22 12:43:27 +0900

    Initial commit

---
+++ src/test/java/com/sparta/awsservice/AwsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.awsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
